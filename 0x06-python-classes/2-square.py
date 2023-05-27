3d06db97869ce7ba56dc67485f5b15d4df41b2c3
aliabdela47 <ardii.2000@gmail.com>
2023-05-27 07:22:39 -0700

    upd

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,45 +1,18 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""A module that defines a square """
+# 2-square.py
+"""Define a class Square."""
 
 
 class Square:
-    """A class that represents a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializing this square class
+        """Initialize a new Square.
         Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
+            size (int): The size of the new square.
         """
-
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size#!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""A module that defines a square """
-
-
-class Square:
-    """A class that represents a square"""
-
-    def __init__(self, size=0):
-        """Initializing this square class
-        Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
-        """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
